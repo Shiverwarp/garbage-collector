f7ee21a0a1618b062ea2498b18265ed8ad4848a9
Shiverwarp <Lorenz69@gmail.com>
2024-02-17 01:52:27 -0700

    Fix extra time

--- packages/garbo/src/diet.ts
+++ packages/garbo/src/diet.ts
@@ -279,7 +279,7 @@ export function nonOrganAdventures(): void {
       const advs = [1, 3, 5][3 - timesUsed];
       return advs * MPA;
     };
-    const extraTimeRemaining = 2 - get("_extraTimeUsed", 3);
+    const extraTimeRemaining = 3 - get("_extraTimeUsed", 3);
     for (let i = extraTimeRemaining; i > 0; i--) {
       if (extraTimeValue(i) > mallPrice($item`extra time`)) {
         if (acquire(1, $item`extra time`, extraTimeValue(i), false)) {
