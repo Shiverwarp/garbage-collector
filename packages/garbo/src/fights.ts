5a4fcb26836c16a910a55b3d9133e0e6c9fe6272
Shiverwarp <Lorenz69@gmail.com>
2024-06-10 05:48:35 -0600

    Don't set familiar for mayflies

--- packages/garbo/src/fights.ts
+++ packages/garbo/src/fights.ts
@@ -2037,7 +2037,6 @@ const freeRunFightSources = [
         const spec: OutfitSpec = {
           equip: $items`mayfly bait necklace, June cleaver`,
           bonuses: new Map([[$item`carnivorous potted plant`, 7000]]),
-          familiar: freeFightFamiliar({ allowAttackFamiliars: false }),
         };
         if (!canPickPocket && bestPickpocketItem) {
           spec.equip?.push(bestPickpocketItem);
