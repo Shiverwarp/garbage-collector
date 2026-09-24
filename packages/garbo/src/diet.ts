ccf1f8fcdd3a4c5b8e7c616cdb594a1e732a6489
Shiverwarp <Lorenz69@gmail.com>
2026-09-23 22:48:06 -0600

    extra hammertime value

--- packages/garbo/src/diet.ts
+++ packages/garbo/src/diet.ts
@@ -206,7 +206,7 @@ function buskEffectValuer(effect: Effect, duration: number): number {
     !have($effect`Hammertime`) &&
     get("_beretBuskingUses") === 0
   ) {
-    return 1_000; // Arbitrary value, assume it will give upcoming busks more value if it's our first busk
+    return 10_000; // Arbitrary value, assume it will give upcoming busks more value if it's our first busk
   }
   return beretEffectValue(effect, duration);
 }
